c9959f594f02294b438565a41a8a15f1af16867d
Aswath Siva <aswathsiva0420@gmail.com>
2026-05-31 10:22:14 +0530

    2.Functional-Components

---
+++ 2.Functional-Components/src/App.jsx
@@ -0,0 +1,12 @@
+function Hello(){
+  return(
+    // <> </> = is used for perfomance optimization - React Fragmentation
+    //          It does not take memory
+    
+    <>
+      <h1>Hii</h1>
+      <h1>Hello world !!</h1>
+    </>
+  )
+}
+export default Hello;
